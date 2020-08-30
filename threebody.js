945b97e0149b24071e68a27f6c7265d661758617
Griffith Young <gryffydd@gmail.com>
2020-08-31 00:29:29 +1000

    bit of a refactor with pan and scale implemented

--- threebody.js
+++ threebody.js
@@ -3,9 +3,10 @@
  *
  */
 
+let SPACE = 1000;
 let MASS = 10;
 let bodies;
-let BODY_COUNT = 3;
+let BODY_COUNT = 33;
 
 let padding = 20;
 let c_width;
@@ -16,42 +17,106 @@ function setup() {
     c_height = windowHeight - padding;
     createCanvas(c_width, c_height);
     background(0);
-    bodies = [];
-    for (b = 0; b < BODY_COUNT; b++) {
-        bodies.push(new Body());
-    }
+    bodies = new Bodies();
 }
 
 function draw() {
     background(color(0, 0, 0, 3));
 
-    for (let b = 0; b < bodies.length; b++) {
-        bodies[b].display();
-    }
+    bodies.display();
+    bodies.move();
+    bodies.apply_gravity();
+    bodies.pan_and_scale();
 
-    for (let b = 0; b < bodies.length; b++) {
-        bodies[b].move();
-    }
-
-    apply_gravity(bodies);
-
-    center(bodies);
+    if (mouseIsPressed) { noLoop(); }
 
     /* Iterate a number of times before stopping - while testing */
     // if (frameCount > 50) { noLoop(); }
 }
 
-let Body = function() {
-    let width = c_width * random(0.05, 0.95);
-    let height = c_height * random(0.05, 0.95);
 
+function nice_color () {
     let r = int(random(64)) + 192;
     let g = int(random(64)) + 192;
     let b = int(random(64)) + 192;
 
-    this.color = color(r, g, b,);
+    return color(r, g, b,);
+}
 
-    this.position = createVector(width, height);
+
+let Bodies = function() {
+    this.scale = 1.0;
+    this.scale = 0.2;
+    this.center = createVector(0, 0);
+
+    this.bodies = [];
+    for (b = 0; b < BODY_COUNT; b++) {
+        this.bodies.push(new Body());
+    }
+
+    this.apply_gravity = function() {
+        let z = createVector(0, 0);
+        for (let i = 0; i < this.bodies.length; i++) {
+            for (let j = 0; j < this.bodies.length; j++) {
+                if (i == j) { continue; }
+                let b1 = this.bodies[i];
+                let b2 = this.bodies[j];
+                let d = b1.position.copy();
+                d.sub(b2.position);
+                let g = (b2.mass / (d.mag() * d.mag())) * SPACE;
+                g = max(min(1, g), 0.00001);
+                d.normalize();
+                d.mult(g);
+                b1.velocity.sub(d);
+            }
+        }
+    }
+
+    this.display = function() {
+        let c = createVector(c_width / 2, c_height / 2);
+        c.sub(this.center);
+        fill(color(0, 51, 0, 17))
+        circle(c.x, c.y, SPACE * 0.5 * this.scale);
+
+        c = createVector(c_width / 2, c_height / 2);
+        for (let i = 0; i < this.bodies.length; i++) {
+            let b = this.bodies[i];
+            let pos = b.position.copy();
+            pos.sub(this.center);
+            pos.mult(this.scale);
+            pos.add(c);
+            fill(b.color);
+            circle(pos.x, pos.y, b.mass * this.scale);
+        }
+    }
+
+    this.move = function() {
+        for (let i = 0; i < this.bodies.length; i++) {
+            this.bodies[i].move();
+        }
+    }
+
+    this.pan_and_scale = function() {
+        let c = createVector(0, 0);
+        let d = SPACE;
+        for (let i = 0; i < this.bodies.length; i++) {
+            c.add(this.bodies[i].position);
+        }
+        c.div(this.bodies.length);
+        for (let i = 0; i < this.bodies.length; i++) {
+            d = max(d, c.dist(this.bodies[i].position));
+        }
+        this.scale = max(0.02, (this.scale + (SPACE / d)) / 2);
+        c.sub(this.center);
+        c.div(5);
+        this.center.add(c);
+    }
+}
+
+
+let Body = function() {
+    this.color = nice_color();
+    this.position = createVector(SPACE * random(-0.5, 0.5), SPACE * random(-0.5, 0.5));
     this.velocity = createVector(random(-1, 1), random(-1, 1));
     this.mass = int(MASS * (1 + random(3)));
 
@@ -69,41 +134,6 @@ let Body = function() {
     }
 }
 
-
-function apply_gravity(bodies) {
-    z = createVector(0, 0);
-    for (let i = 0; i < bodies.length; i++) {
-        for (let j = 0; j < bodies.length; j++) {
-            if (i == j) { continue; }
-            let b1 = bodies[i];
-            let b2 = bodies[j];
-            let d = b1.position.copy();
-            d.sub(b2.position);
-            // let g = (b2.mass / (d.dist(z) * d.dist(z))) * 1000;
-            // if (g > 1) { console.log(i, j, g); }
-            // g = min(1, g);
-            d.normalize();
-            // d.mult(g);
-            d.div(b1.mass / MASS);
-            b1.velocity.sub(d);
-        }
-    }
-}
-
-function center(bodies) {
-    c = createVector(c_width / 2, c_height / 2);
-    bc = createVector(0, 0);
-    for (let b = 0; b < bodies.length; b++) {
-        bc.add(bodies[b].position);
-    }
-    bc.div(bodies.length);
-    if (bc.x < 0 || bc.x > c_width || bc.y < 0 || bc.y > c_height) {
-        bc.sub(c);
-        for (let b = 0; b < bodies.length; b++) {
-            bodies[b].position.sub(bc);
-        }
-    }
-}
 
 /*
  * ---- End of Javascript ----
